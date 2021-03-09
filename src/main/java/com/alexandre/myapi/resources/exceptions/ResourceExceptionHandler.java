4fc91da9cf6d4be6e315b5823913f6c4d1397dfb
alexandre251387 <ajfs2513@gmail.com>
2021-03-09 20:33:39 -0300

    Projeto crud para cadastro de usuarios

---
+++ src/main/java/com/alexandre/myapi/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.alexandre.myapi.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.alexandre.myapi.services.exceptions.ObjectNotFoundExceptions;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundExceptions.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundExceptions e, HttpServletRequest request){
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), System.currentTimeMillis(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+}
